707527960d8abb9c5839a4dc385d63fbf93673a0
IronCoins <vlavatskiy07@bk.ru>
2020-10-30 16:00:12 +0700

    Updates

---
+++ precache-manifest.a7176e82b367d5ba6fba86bb98317cd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96b1a09b81de3dbc5ab503500f34a0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f57dc8f8f5bfe62ed400",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "60ef0882ea9b5e8f24f4",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f57dc8f8f5bfe62ed400",
+    "url": "./static/js/2.41a3c0de.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.41a3c0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ef0882ea9b5e8f24f4",
+    "url": "./static/js/main.bdb84ab0.chunk.js"
+  },
+  {
+    "revision": "e3bd63209c412d72106a",
+    "url": "./static/js/runtime-main.e6a8b0a5.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
